ee7aa186b0ce194c432566b2bf476429352f9c89
Axel <axel.lepine2@gmail.com>
2023-01-22 12:58:31 +0100

    database setup

--- championpoolvalidator/settings.py
+++ championpoolvalidator/settings.py
@@ -79,6 +79,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'blrhwuoaxel',
+        'PASSWORD': 'Nobruteforcepls123',
+        'HOST': 'localhost',
+        'PORT': 3306
     }
 }
 
